5b1cbde68235abdbcfe9b445d0cbd2ade1b96edc
vipul.potdar <vipul.potdar@VIPO-BLR-01.accolite.com>
2018-08-13 17:50:42 +0530

    adding middlelayer classes

---
+++ src/main/java/com/af/prud/model/middleware/LifeDetails.java
@@ -0,0 +1,17 @@
+package com.af.prud.model.middleware;
+
+import java.math.BigInteger;
+
+public class LifeDetails {
+    private String lifeId;
+    private String lifeParty;
+    private BigInteger anbAge;
+    private BigInteger dateOfBirth;
+    private String occupationCode;
+    private String relationshipToLifeInsured;
+    private String selection;
+    private String sex;
+    private String smokingIndicator;
+    private BigInteger height;
+    private BigInteger weight;
+}
---
+++ src/main/java/com/af/prud/model/middleware/OwnerDetails.java
@@ -0,0 +1,8 @@
+package com.af.prud.model.middleware;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class OwnerDetails {
+     private String ownerentId;
+     private String ownerParty;
+}
