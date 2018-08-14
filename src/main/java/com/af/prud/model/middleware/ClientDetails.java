472c21d6d7b772712b9fdc22b2c9a17db43440fb
Abhijeet Saraogi <abhijeet.saraogi@accoliteindia.com>
2018-08-14 13:16:42 +0530

    added clientDetails for middleware objects

---
+++ src/main/java/com/af/prud/model/middleware/ClientDetails.java
@@ -0,0 +1,43 @@
+package com.af.prud.model.middleware;
+
+import java.math.BigInteger;
+
+public class ClientDetails {
+     protected String entId;
+     protected String party;
+     protected String clientNumber;
+     protected String clientType;
+     protected String addressType;
+     protected String birthPlace;
+     protected String address01;
+     protected String address02;
+     protected String address03;
+     protected String address04;
+     protected BigInteger dateOfBirth;
+     protected String postalCode;
+     protected String phone01;
+     protected String phone02;
+     protected String gender;
+     protected String countryCode;
+     protected String directMailIndicator;
+     protected String documentNumber;
+     protected String email;
+     protected String ethnicOrigin;
+     protected String givenName;
+     protected String language;
+     protected String mailingIndicator;
+     protected String marriedIndicator;
+     protected String middleName01;
+     protected String middleName02;
+     protected String nationality;
+     protected String occupationCode;
+     protected String prefCom;
+     protected String salutaion;
+     protected String securityNumber;
+     protected String sourceOfEvidence;
+     protected String statusCode;
+     protected String surName;
+     protected String vipIndicator;
+     protected String forTheAttentionOf;
+     protected String corporateName;
+}
