e9c651f8af96cb38558ca49e01cf0cbda8491919
vipul.potdar <vipul.potdar@VIPO-BLR-01.accolite.com>
2018-08-14 10:33:10 +0530

    rider class

---
+++ src/main/java/com/af/prud/model/middleware/RiderDetails.java
@@ -0,0 +1,17 @@
+package com.af.prud.model.middleware;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class RiderDetails {
+     private String riderId;
+     private String riderParent;
+     private String mortalityClass;
+     private BigInteger premiumCessationAge;
+     private BigInteger premiumCessationTerm;
+     private BigInteger riskCessationAge;
+     private BigInteger riskCessationTerm;
+     private BigInteger sumInsured;
+     private BigDecimal coverageOrRiderInstalmentAmount;
+     private String coverageOrRiderTable;
+}
