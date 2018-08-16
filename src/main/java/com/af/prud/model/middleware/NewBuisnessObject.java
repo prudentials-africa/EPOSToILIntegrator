cfadce33ac26e11d12cfb7a156ca3bbe1a421d2c
Abhijeet Saraogi <abhijeet.saraogi@accoliteindia.com>
2018-08-16 14:45:50 +0530

    added newBuisnessObject

---
+++ src/main/java/com/af/prud/model/middleware/NewBuisnessObject.java
@@ -0,0 +1,28 @@
+package com.af.prud.model.middleware;
+
+public class NewBuisnessObject {
+
+	private ApplyCashDetails applyCashDetails;
+	private AssigneeDetails assigneeDetails;
+	private Assignees assignees;
+	private BankDetails bankDetails;
+	private Beneficiary beneficiary;
+	private ClientDetails clientDetails;
+	private ContractDetails contractDetails;
+	private CoverageDetails coverageDetails;
+	private DispatchDetails dispatchDetails;
+	private DoctorDetails doctorDetails;
+	private FollowUps followUps;
+	private FundDetails fundDetails;
+	private JointOwnerDetails jointOwnerDetails;
+	private LifeDetails lifeDetails;
+	private MandateDetails mandateDetails;
+	private OwnerDetails ownerDetails;
+	private PayerDetails payerDetails;
+	private PremiumRCTDetails premiumRCTDetails;
+	private QuestionDetails questionDetails;
+	private RiderDetails riderDetails;
+	private SFLDetails sflDetails;
+	private SpecialTerms specialTerms;
+
+}
