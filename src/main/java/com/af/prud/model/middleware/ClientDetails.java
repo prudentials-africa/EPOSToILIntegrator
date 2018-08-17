835e69ac0102cd2279fff1030bbd87b5953433b9
Abhijeet Saraogi <abhijeet.saraogi@accoliteindia.com>
2018-08-17 11:32:56 +0530

    updated added getter and setters

--- src/main/java/com/af/prud/model/middleware/ClientDetails.java
+++ src/main/java/com/af/prud/model/middleware/ClientDetails.java
@@ -3,7 +3,44 @@ package com.af.prud.model.middleware;
 import java.math.BigInteger;
 
 public class ClientDetails {
-     public String getEntId() {
+     private String entId;
+     private String party;
+     private String clientNumber;
+     private String clientType;
+     private String addressType;
+     private String birthPlace;
+     private String address01;
+     private String address02;
+     private String address03;
+     private String address04;
+     private BigInteger dateOfBirth;
+     private String postalCode;
+     private String phone01;
+     private String phone02;
+     private String gender;
+     private String countryCode;
+     private String directMailIndicator;
+     private String documentNumber;
+     private String email;
+     private String ethnicOrigin;
+     private String givenName;
+     private String language;
+     private String mailingIndicator;
+     private String marriedIndicator;
+     private String middleName01;
+     private String middleName02;
+     private String nationality;
+     private String occupationCode;
+     private String prefCom;
+     private String salutaion;
+     private String securityNumber;
+     private String sourceOfEvidence;
+     private String statusCode;
+     private String surName;
+     private String vipIndicator;
+     private String forTheAttentionOf;
+     private String corporateName;
+	public String getEntId() {
 		return entId;
 	}
 	public void setEntId(String entId) {
@@ -225,41 +262,5 @@ public class ClientDetails {
 	public void setCorporateName(String corporateName) {
 		this.corporateName = corporateName;
 	}
-	private String entId;
-     private String party;
-     private String clientNumber;
-     private String clientType;
-     private String addressType;
-     private String birthPlace;
-     private String address01;
-     private String address02;
-     private String address03;
-     private String address04;
-     private BigInteger dateOfBirth;
-     private String postalCode;
-     private String phone01;
-     private String phone02;
-     private String gender;
-     private String countryCode;
-     private String directMailIndicator;
-     private String documentNumber;
-     private String email;
-     private String ethnicOrigin;
-     private String givenName;
-     private String language;
-     private String mailingIndicator;
-     private String marriedIndicator;
-     private String middleName01;
-     private String middleName02;
-     private String nationality;
-     private String occupationCode;
-     private String prefCom;
-     private String salutaion;
-     private String securityNumber;
-     private String sourceOfEvidence;
-     private String statusCode;
-     private String surName;
-     private String vipIndicator;
-     private String forTheAttentionOf;
-     private String corporateName;
+     
 }
